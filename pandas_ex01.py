d839cb32ce364e3bbe5b686d011a1d0054990390
hyeongraekim <soboru1002@gmail.com>
2021-07-01 01:04:57 +0900

    practice pandas - dataframe, head, shape

--- pandas_ex01.py
+++ pandas_ex01.py
@@ -1,4 +1,11 @@
-#pandas
+# pandas
 
 import pandas as pd
 
+df=pd.read_csv('hello.csv')
+print(df)
+
+print(df.head(1))
+print(df.shape)  # row, column(행, 열)
+print(df.shape[0])  # row 행개수
+print(df.shape[1])  # column 열개수
