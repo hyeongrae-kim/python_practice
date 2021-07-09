40ee60254d60f568f4c74b2e876d4fc7271f36b9
hyeongraekim <soboru1002@gmail.com>
2021-07-09 16:02:42 +0900

    pandas practice

--- pandas_ex02.py
+++ pandas_ex02.py
@@ -11,3 +11,21 @@ df2 = pd.DataFrame({'A': 1.,
 
 print(df2.dtypes)  # df2의 각 칼럼들 데이터 형식 출력
 
+print(df.head(2))  # 첫 2행 출력
+print(df.tail(2))  # 끝 2행 출력
+
+print(df.index)  # index(index 속성) columns(columns 속성) values(value 속성)
+
+print(df.describe())  # df의 간단한 컬럼별 통계정보를 df 형태로 보여줌
+
+df = df.T  # df의 행과 열을 바꿔줌
+
+print(df)
+
+df['A']  # 특정 칼럼 선택하는 방법
+df[0:3]  # 특정 행 선택하는 방법
+
+df.loc['20210701']  # 행 이름으로 특정 행 선택해서 가져오는 방법
+
+df.loc[:, ['A', 'B']]  # 칼럼 a와 b에 해당되는 모든 값 가져오기
+
